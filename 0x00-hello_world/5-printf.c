eb05e197391fcb158ccd7d423b0573a3b5f66a83
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-08 09:39:35 -0700

    still on it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 	/**
-	 * main- Entry point
-	 * Description: prints "with proper"
-	 * Return: Always 0 (success);
+	 * main - Entry point
+	 * Description: prints "with proper grammar, but the outcome is a piece of art,"
+	 * Return: Always 0 (Success);
 	 */
 	int main(void)
 	{
