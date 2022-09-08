718724651f575935859d5b6ff12ac17e0d2633c1
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-08 09:36:36 -0700

    so tiring

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 	/**
 	 * main- Entry point
-	 * Description: printf-printing command
+	 * Description: prints "with proper"
 	 * Return: Always 0 (success);
 	 */
 	int main(void)
