d29131a81239ef4e40dd0fd85a8d56f45f68a38d
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-08 09:20:28 -0700

    still on printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-	/* All about main */
+	/*
+	 * main- Entry point
+	 * printf-printing command
+	 */
 	int main(void)
 	{
 		/* ALl about Printing */
-		printf("with proper grammar, but the outcome is a piece of art, ");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 	}
