6d752f3144542d366d561c0d0460dc5c69f85336
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-08 09:15:54 -0700

    first printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+	/* All about main */
 	int main(void)
 	{
 		/* ALl about Printing */
-		printf("with proper grammar, but the outcome is a piece of art,");
-		return 0;
+		printf("with proper grammar, but the outcome is a piece of art, ");
+		return (0);
 	}
