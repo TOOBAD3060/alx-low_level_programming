f03809b5ce54aa6156d1eda2de3348a33c22d258
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-13 03:55:23 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- prints
+ * Return : 0 (Always success)
+ */
+void print_alphabet(void)
+{
+	char ch='a';
+
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
+
