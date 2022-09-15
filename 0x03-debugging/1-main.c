220f015ffab71f3bc442082ec8bd1bb2dac782cd
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-15 14:32:46 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,6 @@
  */
 
 int main(void)
-
 {
 	int i;
 
@@ -15,19 +14,11 @@ int main(void)
 	i = 0;
 
 	/*while (i < 10)*/
-
 	/*{*/
-
 	/*putchar(i);*/
-
 	/*}*/
-
-
 
 	printf("Infinite loop avoided! \\o/\n");
 
-
-
 	return (0);
-
 }
