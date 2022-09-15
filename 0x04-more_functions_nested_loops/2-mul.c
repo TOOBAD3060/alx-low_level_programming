79c3dfc812d8e6561941ed6e605079a70c4e1291
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-15 08:51:25 -0700

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multipy two integers
  * @a: integer to be multiplied to b
  * @b: integer to be multiplied to a
- * Return: 0 (always success)
+ * Return: always success
  */
 
 int mul(int a, int b)
