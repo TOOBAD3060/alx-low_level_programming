758b1362a237b9a0187b65ee6fa05d55684a9fd1
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-27 09:43:52 -0700

    correct memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,15 +5,13 @@
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
 
 	unsigned int i;
-
-
 
 	for (i = 0; i < n; i++)
 
@@ -23,8 +21,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	}
 
-
-
 	return (dest);
-
 }
