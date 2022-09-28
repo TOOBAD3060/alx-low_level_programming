961e166639ebf15b8ce71100fe098e19efbbfee8
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-28 10:19:35 -0700

    square recurssion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,17 +6,19 @@
  * Return: -1 if n has nor square root
  */
 
+int square_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+	if (root * root == n)
+		return root;
+	return (square_root(n, root + 1));
+}
+
+
 int _sqrt_recursion(int n)
 {
-	int root = 0;
-
-	if ((root * root) == n)
-	{
-		return root;
-		root++;
-	}
-	else
-	{
+	if (n < 0)
 		return (-1);
-	}
+	return (square_root(n, 0));
 }
