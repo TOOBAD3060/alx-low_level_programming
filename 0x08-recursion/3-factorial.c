4df0c380df2c017aa2281a17c3e22167175b0362
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-09-28 09:15:58 -0700

    factorial betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,10 +2,11 @@
 
 /**
  * factorial - function that returns the factorial of a given number
- * @n : numbers whose factorial is to be determined
- * Return (-1) when n is less than 0
- * Return (1) when n is 0
+ * @n: numbers whose factorial is to be determined
+ * Return: (-1) when n is less than 0,
+ * 1 when n is 0
  */
+
 int factorial(int n)
 {
 	if (n < 0)
@@ -14,5 +15,3 @@ int factorial(int n)
 		return (1);
 	return (n * factorial(n-1));
 }
-	
-		
