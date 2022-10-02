8008f354cd98c0670a64907383aba20ea836ec03
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-10-02 08:44:29 -0700

    total args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: total number of arguments
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+
+	return (0);
+}
+
