aa676669cf39552f8bbdb5aa21c202ed02692d98
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-10-02 10:44:06 -0700

    multi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,14 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int res;
 
-	if (argc == 3)
-	{
-		res = atoi(argv[1]) * atoi(argv[2]);
-
-		printf("%d\n", res);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
