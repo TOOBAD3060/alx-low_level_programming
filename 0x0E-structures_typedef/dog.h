a5c5967e0594fd4ab6e5c3445de767f345268655
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-10-10 20:08:20 -0700

    new dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog
-{
-	char* name;
+/**
+ * struct dog - All about dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog {
+	char *name;
 	float age;
-	char* owner;
-}
+	char *owner;
+};
+
 
 #endif
