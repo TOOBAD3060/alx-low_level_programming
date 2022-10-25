3b0f6508387a2b3a8fad1e9077832593bef5c172
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2022-10-25 14:39:27 -0700

    find loop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,25 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-
 	size_t nnodes = 0;
 
-
-
 	while (h != NULL)
-
 	{
-
 		printf("%d\n", h->n);
-
 		h = h->next;
-
 		nnodes++;
-
 	}
-
 	return (nnodes);
 
 }
-
-
